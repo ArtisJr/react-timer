05859e44e55bd176eef56f16562e64fc5ab2ff9c
Artis Coleman <ArtisColemanJr@gmail.com>
2017-05-09 13:04:22 -0400

    Create CountdownForm

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
             Main: 'app/components/Main.jsx',
             Timer: 'app/components/Timer.jsx',
             Countdown: 'app/components/Countdown.jsx',
+            CountdownForm: 'app/components/CountdownForm.jsx',
             Clock: 'app/components/Clock.jsx',
             Navigation: 'app/components/Navigation.jsx',
             applicationStyles: 'app/styles/app.scss'
