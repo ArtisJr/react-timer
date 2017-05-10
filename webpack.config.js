b6361776c22f3f4feddb39919bbc2e68e345711e
Artis Coleman <ArtisColemanJr@gmail.com>
2017-05-10 15:31:47 -0400

    Wire up controls component

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
             Timer: 'app/components/Timer.jsx',
             Countdown: 'app/components/Countdown.jsx',
             CountdownForm: 'app/components/CountdownForm.jsx',
+            Controls: 'app/components/Controls.jsx',
             Clock: 'app/components/Clock.jsx',
             Navigation: 'app/components/Navigation.jsx',
             applicationStyles: 'app/styles/app.scss'
